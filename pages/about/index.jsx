b1e39ebaac73a710f87b379ee5b1310f1dad6001
sana <sanasafa09206@gmail.com>
2025-06-11 15:49:41 +0530

    message

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,67 +1,10 @@
+import Loginform from '@/public/components/form/Loginform'
 import React from 'react'
+import {userState} from 'react'
 
 function index() {
-  import Head from "next/head";
-  import Image from "next/image";
-  import { Geist, Geist_Mono } from "next/font/google";
-  import styles from "@/styles/Home.module.css";
-  import React ,{useState} from "react";
-  
-  const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-  });
-  
-  const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-  });
-  
-  export default function Home() {
-    const [state,setState]=useState("");
-    console.log("state",state);
-    const[password,setPassword]=useState("");
-    console.log("password", password);
-    const[email,setEmail]=useState("");
-    console.log("email", email);
-  function handleState(event){
-    setState(event.target.value);
-  }
-  function handlePass(event){
-   setPassword(event.target.value);
-  
-  }
-  function handleEmail(event){
-   setEmail(event.target.value);
-  }
-  function handleSubmit(event){
-    if (password.length < 8){
-      alert("password must contain atleast 8 characters");
-      return;
-    }
-    console.log("submitted")
-    }
-  
   return (
-    <div>
-    <h1>
-    hey
-    </h1>
-<p>enter your name</p><input type= "text"onChange={handleState}/>
-<p>enter your email</p><input type= 'text'onChange={handleEmail}/>
-<button onClick={()=>setState("hello")}>.    {state}</button>
-<div>
-
-</div>
-<h1>
-<input type='text'onChange={handleState}/>
-</h1>
-  <p>enter your password</p><input type='password'onChange={handlePass}/>  
-
-   <button onClick={handleSubmit}>Submit</button>    
-  </div>  
-  );  
-    <div>index</div>
+    <Loginform/>
   )
 }
 
