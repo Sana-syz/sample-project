48925f787d6a65d3dc08507901f0659a57b6da05
sana <sanasafa09206@gmail.com>
2025-06-11 15:12:47 +0530

    message

---
+++ components/Loginform.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Loginform from '../components'
+import index from '@/pages/about'
+function Loginform() {
+  return (
+    <div>Loginform</div>
+  )
+}
+
+export default index
